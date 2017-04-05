43f464edc8c1ea41cdc2254c959cda6438996056
Willian Frantz <willian.frantz@deway.com.br>
2017-04-05 12:45:47 -0300

    Add Admin user hardcoded on seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(name: 'Admin', email: 'admin@strv.com', password: 'password', password_confirmation: 'password')
+user.add_roles "admin"
