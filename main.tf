0febf80953de8bdd23053a2084d84ee70dc27da0
WLSF <willianluigii@hotmail.com>
2019-07-01 21:03:03 -0300

    :wrench: Remove secret env vars

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ resource "heroku_app" "default" {
 
     config_vars = {
       RAILS_ENV = "production"
-      FIREBASE_URI = "https://addressbook-contacts.firebaseio.com"
-      FIREBASE_SECERT_KEY = "AIzaSyCsS2AcN4yZY8iQwJGtph29PWHcQd3tsl4"
+      FIREBASE_URI = ""
+      FIREBASE_SECERT_KEY = ""
     }
 }
 
